52802edbcfc06fa51f0d6c12ff139366e3afc549
div-antk <51283468+owaii@users.noreply.github.com>
2022-08-07 21:18:36 +0900

    リンゴに当たり判定を追加

--- Assets/@Project/Scripts/Fruit.cs
+++ Assets/@Project/Scripts/Fruit.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class Fruit : MonoBehaviour
 {
     //他のプロジェクトとぶつかったときに呼び出される関数
-    private void void OnTriggerEnter2D(Collider2D other)
+    private void OnTriggerEnter2D(Collider2D other)
     {
         //名前に "Player" が含まれるオブジェクトとぶつかったら
         if (other.name.Contains("Player"))
