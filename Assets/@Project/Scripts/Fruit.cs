313644cba27f0e0f1f52444be359aa2d4d407b01
div-antk <51283468+owaii@users.noreply.github.com>
2022-08-07 21:12:52 +0900

    リンゴにスクリプトを適用

---
+++ Assets/@Project/Scripts/Fruit.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fruit : MonoBehaviour
+{
+    //他のプロジェクトとぶつかったときに呼び出される関数
+    private void void OnTriggerEnter2D(Collider2D other)
+    {
+        //名前に "Player" が含まれるオブジェクトとぶつかったら
+        if (other.name.Contains("Player"))
+        {
+            //自分自身を削除
+            Destroy(gameObject);
+        }
+    }
+}
