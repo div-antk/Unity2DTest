84882fb39d78d31c00ddd7bff5b46ebe3921dfc7
div-antk <51283468+owaii@users.noreply.github.com>
2022-08-19 01:13:55 +0900

    針に当たるとやられアニメーションを再生

--- Assets/@Project/Scripts/Spike.cs
+++ Assets/@Project/Scripts/Spike.cs
@@ -5,6 +5,9 @@ using UnityEngine.SceneManagement;
 
 public class Spike : MonoBehaviour
 {
+    // プレイヤーのやられアニメーションのプレハブ
+    public PlayerHit m_playerHitPrefab;
+
     private void OnTriggerEnter2D(Collider2D other) {
         if (other.name.Contains("Player"))
         {
@@ -17,12 +20,21 @@ public class Spike : MonoBehaviour
         
             // 2秒後にリトライする
             Invoke("onRetry", 2);
+
+            // このプレイヤーのやられアニメーションのオブジェクトを生成
+            Instantiate
+            (
+                m_playerHitPrefab,
+                other.transform.position,
+                Quaternion.identity
+            );
         }
     }
 
     // リトライするたびに呼び出される関数
     private void onRetry()
     {
+        // 現在のシーンを読み込み直してリトライする
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 }
