6c62e466fe1e9e33f32574e757999b54726ea98d
div-antk <51283468+owaii@users.noreply.github.com>
2022-08-21 16:48:12 +0900

    トランポリンのスクリプトを作成してアタッチ

---
+++ Assets/@Project/Scripts/Trampoline.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trampoline : MonoBehaviour
+{
+    // プレイヤーをジャンプさせる高さ
+    public float m_jumpHeight = 10;
+
+    // 他のオブジェクトと当たったときに呼び出される関数
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        // 名前に Player が含まれるオブジェクトと当たったら
+        if (other.name.Contains("Player"))
+        {
+            // プレイヤーの操作を制御するスクリプトを取得して
+            var motor = other.GetComponent<PlatformerMotor2D>();
+
+            // プレイヤーをジャンプさせる
+            motor.ForceJump(m_jumpHeight);
+        }
+    }
+}
