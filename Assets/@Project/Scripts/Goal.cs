1574eaa65a47e1694f6f0eed25df3a604176ea31
div-antk <51283468+owaii@users.noreply.github.com>
2022-09-29 23:53:54 +0900

    ゴールのスクリプトを記述してアタッチ

---
+++ Assets/@Project/Scripts/Goal.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Goal : MonoBehaviour
+{
+    // ゴールしたかどうか
+    private bool m_isGoal;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (!m_isGoal)
+        {
+            if (other.name.Contains("Player"))
+            {
+                var cameraShake = FindObjectOfType<CameraShaker>();
+
+                cameraShake.Shake();
+
+                m_isGoal = true;
+            }
+        }
+    }
+
+}
