815de3797da66192f35d6583334ef90e023b6200
div-antk <51283468+owaii@users.noreply.github.com>
2022-10-09 23:58:48 +0900

    Playerスクリプトの修正

--- Assets/@Project/Scripts/Player.cs
+++ Assets/@Project/Scripts/Player.cs
@@ -1,3 +1,4 @@
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,6 +7,28 @@ using UnityEngine.SceneManagement;
 public class Player : MonoBehaviour
 {
     public GameObject m_playerHitPrefab;
+
+    // ジャンプした時に再生する SE
+    public AudioClip m_jumpClip;
+
+
+    // シーンが開始する時に呼び出される関数
+    private void Awake()
+    {
+        // プレイヤーの移動を制御するコンポーネントを取得する
+        var motor = GetComponent<PlatformerMotor2D>();
+
+        // ジャンプした時に呼び出されるイベントに関数を登録する
+        motor.onJump += OnJump;
+    }
+
+    // ジャンプした時に呼び出される関数
+    private void OnJump()
+    {
+        // ジャンプした時の SE を再生する
+        var audioSource = FindObjectOfType<AudioSource>();
+        audioSource.PlayOneShot( m_jumpClip );
+    }
 
     // プレイヤーがやられたときに呼び出す関数
     public void Dead()
@@ -33,6 +56,20 @@ public class Player : MonoBehaviour
         );
     }
 
+    private AudioClip m_jumpClip;
+
+     private void Awake() {
+        var motor = GetComponent<PrivateMoror2D>();
+
+        motor.onJump += OnJump()     
+    }
+
+private void OnJump()
+{
+    var audioSource = FindObjectOfType<audioSource>();
+    audioSource.PlayOnShot(m_playerHitPrefab);
+}
+
     // リトライするたびに呼び出される関数
     private void onRetry()
     {
@@ -40,3 +77,4 @@ public class Player : MonoBehaviour
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 }
+
